315a50713c84f4a2ebd1f086648afca8576f6789
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-03-20 18:42:26 +0000

    removed margin-block start and end

--- src/index.styled.jsx
+++ src/index.styled.jsx
@@ -4,6 +4,8 @@ export const Body = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
+        margin-block-start: 0;
+        margin-block-end: 0;
         box-sizing: border-box;
     }
 `;
