20ae07a9ee204b93e1558ea88298561621ba0976
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-03-20 19:10:00 +0000

    added h1 font-family

---
+++ src/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const H1Text = styled.h1`
+  font-family: "IBM Plex Sans Devanagari";
+`;
+
+export const PText = styled.p`
+  fontsize: var(--font-size, 14px);
+`;
