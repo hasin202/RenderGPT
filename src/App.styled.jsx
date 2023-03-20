0731f0bcdca8cec38002e2d395be3d4d8544545b
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-03-20 19:12:52 +0000

    made PageContaier styled component

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export const PageContainer = styled.div`
+  max-width: 1000px;
+`;
+
 export const H1Text = styled.h1`
   font-family: "IBM Plex Sans Devanagari";
 `;
