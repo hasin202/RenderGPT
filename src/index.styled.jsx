f2b527a0db5656c496fbcca4ac7aee0edd4c5443
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-03-20 19:17:45 +0000

    added styling for body

--- src/index.styled.jsx
+++ src/index.styled.jsx
@@ -12,5 +12,9 @@ export const Body = createGlobalStyle`
     body {
         display: flex;
         flex-direction: column;
+        align-items:center;
+
+        margin-top: 50px;
+        margin-bottom: 50px
     }
 `;
